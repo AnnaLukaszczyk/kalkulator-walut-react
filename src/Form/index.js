ea73db7cbd4baeccf0bed9774ba70422ef694b86
Anna Lukaszczyk <normalnie4@tlen.pl>
2023-10-04 14:06:28 +0200

    Currency converter without interactions yet

---
+++ src/Form/index.js
@@ -0,0 +1,40 @@
+import "./style.css";
+
+const Form = () => (
+	<form className="form">
+		<p>
+			<label className="form__labelText">Waluta, którą posiadasz:</label>
+			<select className="form__currency">
+				<option>PLN</option>
+				<option>EUR</option>
+				<option>GBP</option>
+				<option>USD</option>
+			</select>
+		</p>
+		<p>
+			<label className="form__labelText">Kwota, którą chcesz przeliczyć:</label>
+			<input
+				className="form__input"
+				type="number"
+				placeholder="Podaj kwotę"
+				step="0.01"
+			/>
+		</p>
+		<p className="form__errorText">Musisz podać kwotę</p>
+		<p>
+			<label className="form__labelText">
+				Waluta, na którą chcesz przeliczyć:
+			</label>
+			<select className="form__currency">
+				<option>PLN</option>
+				<option>EUR</option>
+				<option>GBP</option>
+				<option>USD</option>
+			</select>
+		</p>
+		<button className="button button--count">Przelicz</button>
+		<button className="button">Wyczyść</button>
+	</form>
+);
+
+export default Form;
---
+++ src/Result/index.js
@@ -0,0 +1,10 @@
+import "./style.css";
+
+const Result = () => (
+	<>
+		<p className="result"></p>
+		<p className="info">Przeliczono po kursie z dnia 28.08.2023</p>
+	</>
+);
+
+export default Result;
