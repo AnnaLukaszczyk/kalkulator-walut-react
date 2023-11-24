61dad84056d8ac7e84dc84dee2f78ace1e25100f
Anna Lukaszczyk <normalnie4@tlen.pl>
2023-11-24 15:29:59 +0100

    added useRatesData

---
+++ src/useRatesData.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const JSON_URL = "https://raw.githubusercontent.com/loca20/kalkulator-walut-react/main/public/currencyRates.json";
+//onst API_URL = "https://api.currencyapi.com/v3/latest?apikey=cur_live_2HBIAKhdpMCHaQBq2UCjf5dGv3esN6WuAhXyyBIq&currencies=&base_currency=PLN"
+
+export const useRatesData = () => {
+	const [ratesData, setRatesData] = useState({ status: "loading", data: null });
+
+	useEffect(() => {
+		setTimeout(() => {
+		(async () => {
+			try {
+				const response = await axios.get(JSON_URL);
+
+				setRatesData({
+					status: "success",
+					data: response.data,
+				});
+			} catch (error) {
+				setRatesData({ status: "error" });
+			}
+		})();
+		}, 2000);
+	}, []);
+
+	return { ratesData };
+};
